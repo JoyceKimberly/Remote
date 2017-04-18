f5bb8b77b171426b61cd152c36b30b9566010af8
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-04-18 19:49:43 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,31 +1,38 @@
+var window.ip = "192.168.0.100";
+var window.port = "8008";
+var window.username = "popcorn";
+var window.username = "password";
+
 $(function() { // -----------------------------------------------------------------------
+
+  callPopcornApi();
 
 }); // ----------------------------------------------------------------------------------
 
 function callPopcornApi(method, params) {       //popcorn api wrapper
-       
-        if(typeof params === "undefined"){
-                params = [];
-        }
- 
-        var request = {};
-       
-        request.params = params;
-        request.id = 10;
-        request.method = method;
-        request.jsonrpc = "2.0";
-       
-         $.ajax({
-            type: 'POST',
-            url: 'http://' + window.ip + ':' + window.port,
-            data: JSON.stringify(request),
-            //dataType: 'json',
-            beforeSend: function (xhr){
-                        xhr.setRequestHeader('Authorization', window.btoa(window.username + ":" + window.password));
-                        },
-                success: function(data, textStatus) {
-                                console.log(data);
-                        },    
-        });
-               
-}
+  
+  if ( typeof params === "undefined" ) {
+    params = [];
+  };
+  
+  var request = {};
+
+  request.params = params;
+  request.id = 10;
+  request.method = method;
+  request.jsonrpc = "2.0";
+  
+  $.ajax({
+    type: 'POST',
+    url: 'http://' + window.ip + ':' + window.port,
+    data: JSON.stringify(request),
+    //dataType: 'json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('Authorization', window.btoa(window.username + ":" + window.password));
+    },
+    success: function(data, textStatus) {
+      console.log(data);
+    },    
+  });
+
+};

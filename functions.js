d99ec34134a9bd1d2dea30688460562d543700ee
Jo Kim <JoyceKimberly@users.noreply.github.com>
2017-04-18 19:40:40 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,31 @@
 $(function() { // -----------------------------------------------------------------------
 
 }); // ----------------------------------------------------------------------------------
+
+function callPopcornApi(method, params) {       //popcorn api wrapper
+       
+        if(typeof params === "undefined"){
+                params = [];
+        }
+ 
+        var request = {};
+       
+        request.params = params;
+        request.id = 10;
+        request.method = method;
+        request.jsonrpc = "2.0";
+       
+         $.ajax({
+            type: 'POST',
+            url: 'http://' + window.ip + ':' + window.port,
+            data: JSON.stringify(request),
+            //dataType: 'json',
+            beforeSend: function (xhr){
+                        xhr.setRequestHeader('Authorization', window.btoa(window.username + ":" + window.password));
+                        },
+                success: function(data, textStatus) {
+                                console.log(data);
+                        },    
+        });
+               
+}
